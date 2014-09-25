b8051e273d483c00777e2f4a8d39a8a37467a4f3
dlandreman <david.landreman@crosschx.com>
2014-09-24 20:54:41 -0400

    - Chat box stuff done

--- Dungeon/dungeon.js
+++ Dungeon/dungeon.js
@@ -5,13 +5,18 @@ app.controller("dungeonController", function($scope,$timeout) {
 	$scope.commands = ["look","move","yell"];
 	$scope.known_commands =[];
 	
+	$scope.results = [];
+	
 	$scope.ref_user_text = function(){
 		console.log($scope.user_text);
 		
-		if(_.contains($scope.commands, $scope.user_text) 
-				&& !_.contains($scope.known_commands,$scope.user_text)) {
-			$scope.known_commands.push($scope.user_text);
+		if(_.contains($scope.commands, $scope.user_text)) {
+			$scope.results.push("You have " + $scope.user_text + "ed!");
+			if(!_.contains($scope.known_commands,$scope.user_text)) {
+				$scope.known_commands.push($scope.user_text);
+			}
 		}
+		$scope.user_text = "";
 	}
 	
 	$scope.boyfriend = "Rus Fillmore!!";
