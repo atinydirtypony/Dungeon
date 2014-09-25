c7bb30274e9dfcdb9d7601e53aa8fa89294ecb80
Rus Fillmore <rx.fillmore@gmail.com>
2014-09-24 20:55:40 -0400

    I added comments for things i want

--- Dungeon/dungeon.js
+++ Dungeon/dungeon.js
@@ -5,6 +5,14 @@ app.controller("dungeonController", function($scope,$timeout) {
 	$scope.commands = ["look","move","yell"];
 	$scope.known_commands =[];
 	
+	//Define Spot in room
+	
+	//Define Room
+	
+	//Generate Map
+	$scope.map_produce = function()
+	
+	//Check to see if the user knows that command and if not add it to list
 	$scope.ref_user_text = function(){
 		console.log($scope.user_text);
 		
@@ -14,7 +22,4 @@ app.controller("dungeonController", function($scope,$timeout) {
 		}
 	}
 	
-	$scope.boyfriend = "Rus Fillmore!!";
-	
-
 });
